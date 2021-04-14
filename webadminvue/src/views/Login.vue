1b24c8450a17b534cc5d431dda15a6d585304f7e
Opshell <phenomx9990@gmail.com>
2021-04-14 22:58:56 +0800

    新增 js-cookie 新增 login 頁面 新增 router 導向前驗證

--- webadminvue/src/views/Login.vue
+++ webadminvue/src/views/Login.vue
@@ -1,7 +1,46 @@
 <template>
-  <div class="login">
-    <h1>This is an about page</h1>
-    <h1>This is an about page</h1>
-    <h1>This is an about page</h1>
-  </div>
+    <div class="row vertical" data-width="20rem">
+        <el-input v-model="loginForm.username" type="text" placeholder="Username" />
+        <el-input v-model="loginForm.password" type="password" placeholder="Password" />
+        <el-button type="primary" @click="handleLogin">Login</el-button>
+        <el-button @click="removeCookie">Clear</el-button>
+    </div>
 </template>
+
+<script>
+    import Cookies from 'js-cookie';
+
+    export default {
+        data() {
+            return{
+                loginForm: {
+                    username: '',
+                    password: '',
+                    token: ''
+                }
+            }
+        },
+        methods: {
+            handleLogin() {
+                const token = 'asdsadsafASFadfsaf';
+                let username = this.loginForm.username;
+                let password = this.loginForm.password;
+
+                if (username !== '' && password !== '') {
+                    this.loginForm.token = token;
+                } else {
+                    alert('帳號密碼不能為空');
+                }
+
+                Cookies.set('login', JSON.stringify(this.loginForm), { expires: 1 });
+                console.log(this.loginForm);
+                if (Cookies.get('login') && this.loginForm.token) {
+                    this.$router.push({name: 'Dashboard'});
+                }
+            },
+            removeCookie() {
+                Cookies.remove('login');
+            }
+        }
+    }
+</script>
